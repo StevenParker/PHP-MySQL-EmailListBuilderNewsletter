6ebeff9e26e9ef4dff2288c789106fea6d2142b8
Steven Parker <PSteion148@Gmail.com>
2014-12-31 02:07:17 +0000

    Create interface.php

---
+++ interface.php
@@ -0,0 +1,17 @@
+<?php
+  require '\sourcecode.php'
+  
+	$email = $_POST['email'];
+	
+	//array(clientemaillist::add_client($email_list));
+
+?>
+
+<html>
+<body>
+
+	<div class "box">
+	</div>
+
+</body>
+</html>
