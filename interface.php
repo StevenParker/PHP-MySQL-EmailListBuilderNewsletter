ff58772c6c935aeeb241953bc049f44706672ff4
Steven Parker <PSteion148@Gmail.com>
2015-01-01 19:40:11 +0000

    Update interface.php
    HTML check boxes and check box toggle with JavaScript.

--- interface.php
+++ interface.php
@@ -1,17 +1,42 @@
 <?php
-  require '\sourcecode.php'
-  
-	$email = $_POST['email'];
-	
-	//array(clientemaillist::add_client($email_list));
-
+require '\sourcecode.php';
 ?>
-
 <html>
+<head>
+	<title>Email Client Builder</title>
+	<link rel="stylesheet" type="text/css" href="interface.css" />
+</head>
 <body>
-
-	<div class "box">
+	<div id="box">
+		<div class="Title">
+		</div>
+		<div class="Table">
+		<fieldset>
+			
+			<h4>Email Client List</h4>
+			<label><input type="checkbox" name="emailaddress" value="bar1"> 40 characters long......................<br/></label>
+			<label><input type="checkbox" name="emailaddress" value="bar2"> 40 characters long......................<br/></label>
+			<label><input type="checkbox" name="emailaddress" value="bar3"> 40 characters long......................<br/></label>
+			<label><input type="checkbox" name="emailaddress" value="bar4"> 40 characters long......................<br/></label>
+			<label><input type="checkbox" name="emailaddress" value="bar5"> 40 characters long......................<br/></label>
+			<label><input type="checkbox" name="emailaddress" value="bar6"> 40 characters long......................<br/></label>
+			<label><input type="checkbox" name="emailaddress" value="bar7"> 40 characters long......................<br/></label>
+			<label><input type="checkbox" name="emailaddress" value="bar8"> 40 characters long......................<br/></label>
+			<p>
+			<label><input type="checkbox" onClick="toggle(this)" /> Toggle All<br />
+			</fieldset>	
+		</div>
 	</div>
-
+	
+	<script>
+	function toggle(source) 
+	{
+		checkboxes = document.getElementsByName('emailaddress');
+		for(var i=0, n=checkboxes.length;i<n;i++) 
+		{
+			checkboxes[i].checked = source.checked;
+		}
+	}
+	</script>
 </body>
 </html>
