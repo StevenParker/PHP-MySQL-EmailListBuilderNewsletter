12dbfcba9ad8cde5f5032442311f1b35c6309681
Steven Parker <PSteion148@Gmail.com>
2015-01-04 00:42:00 +0000

    Update sourcecode.php

--- sourcecode.php
+++ sourcecode.php
@@ -1,28 +1,110 @@
 <?php
+class clientemaillist
+{
+	public $dbconnection;
+	public $dbselect;
+	public $tableselect;
+	public $column1;
+	public $column2;
+	public $column3;
+	public $forename;
+	public $surname;
+	public $emailaddress;
+	public $queryresults;
+	public $result;
+	public $rowcheck;
 
-	class clientemaillist
-	{	
-		function __construct() 
+	public function __construct() 
 		{
-			echo 'In clientemaillist constructor\n';
+			echo 'In clientemaillist constructor';
 		}
 		
-		function __destruct()
+	public function __destruct()
 		{
-			echo 'In clientemaillist destruct\n';
+			echo 'In clientemaillist destruct';
 		}
-	}
-	
-	class editionlistfunctions extends clientemaillist
+}
+
+//dbcm = database connection methods
+class dbcm extends clientemaillist
+{
+	function databaseconnection($dbconnection)
 	{
-		public $clients;	
-			
-		function add_client($email, $emaillist)
+		global $dbconnection;
+		$dbconnection = mysql_connect($hostname, $username, $password);
+
+		if ($dbconnection->connect_error)
+		{
+			die("Connection Failed: " . $dbconnection->connect_error);
+		}
+
+		$objdbconnect = new dcbm;
+		$objdbconnect->databaseconnection($dbconnection = mysql_connect($hostname, $username, $password))
+	}
+
+	function databaseselect($dbselect)
+	{
+		global $dbselect;
+		$dbselect = mysql_select_db($dbname, $dbconnection);
+
+		$objselect = new dcbm;
+		$objselect->databaseselect($dbselect = mysql_select_db($dbname, $dbconnection));
+	}
+
+	function tablequery($tableselect)
+	{
+		
+
+		if($dbconnection->query($tableselect) === TRUE)
+		{
+			echo "New record created successfully";
+		}
+		else
+		{
+			echo "Error" . $tablequery . "<br>" . $dbconnection->error;
+		}
+
+		global $tableselect;
+		$tableselect = "INSERT INTO $dbname	($column1, $column2, $column3) 
+						VALUES 				($forename, $surname, $emailaddress)";
+
+		if($dbconnection->tablequery($tableselect) === TRUE)
+		{
+			echo "New record created successfully";
+		}
+		else
+		{
+			echo "Error: " . $tableselect . "<br>" . $dbconnection->error;
+		}
+
+		$objquery = new dbcm;
+		$objquery->tablequery($tableselect = "INSERT INTO $dbname		($column1, $column2, $column3) 
+											VALUES 					($forename, $surname, $emailaddress)");
+	}
+
+	function displayresult($queryresults)
+	{
+		global $queryresults;
+		$queryresults = mysql_query($tableselect);
+	}
+
+	function numrows($rowcheck)
+	{
+		global $rowcheck;
+		$rowcheck = mysql_num_rows($queryresults);
+	}
+}
+
+/*
+class editionlistfunctions extends clientemaillist
+	{
+
+		public function add_client($email, $emaillist)
 		{
 			$this->clients[$email] += $emaillist;
 		}
-		
-		function remove_client($email, $emaillist)
+
+		public function remove_client($email, $emaillist)
 		{
 			if($this->clients[$email] > $emaillist)
 			{
@@ -40,29 +122,5 @@
 			}
 		}
 	}
-	
-	class database extends clientemaillist
-	{
-		function mysql_connect($hostname, $username, $password){;}
-
-		function mysql_select_db($dbname, $dbconnection){;}
-	}
-	
-	class databasedatapull extends database
-	{
-		function db_data_pull()	
-	}
-		
-	$addclient = new editionlistfunctions();
-	$addclient->add_client($email, $emaillist);
-
-	$removeclient = new editionlistfunctions();
-	$removeclient->remove_client($email, $emaillist);
-	
-	$dbconnection = new database();
-	$dbconnection->mysql_connect($hostname, $username, $password);
-	
-	$dbselect = new database();
-	$dbselect->mysql_select_db($dbname, $dbconnection);
-	
+*/
 ?>
