b7a7325db5e72427b60e8e80301ef2d6498a1522
Steven Parker <PSteion148@Gmail.com>
2015-01-04 00:44:53 +0000

    Create forminterface.php

---
+++ forminterface.php
@@ -0,0 +1,12 @@
+<html>
+	<body>
+		<form action="interface.php" method="POST">
+			<p>Forename:</p><input type="text" name="forename"/>
+			<p>Surname:</p><input type="text" name="surname"/>
+			<p>Email Address:</p><input type="text" name="emailaddress"/>
+			<br />
+			<br>
+			<input type="submit" value="submit" />
+			</form>		
+		</body>
+</html>
